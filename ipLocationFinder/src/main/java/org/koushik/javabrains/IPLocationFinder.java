8bb7e8409481a19bf4fef01f84863b34b95ba0be
alitvin <litwinoff@gmail.com>
2017-01-23 09:02:42 +0300

    Client works! (even with proxy)

--- ipLocationFinder/src/main/java/org/koushik/javabrains/IPLocationFinder.java
+++ ipLocationFinder/src/main/java/org/koushik/javabrains/IPLocationFinder.java
@@ -1,15 +1,34 @@
 package org.koushik.javabrains;
 
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
+
 public class IPLocationFinder {
 
 	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("You need to pass in one IP address");
-		} else {
-			// calling web service here ...
-			String ipAddress = args[0];
-			stub.getCountryName(ipAddress);
-		}
+		
+		// Setting proxy settings inside Java code
+		String proxyAddr = "proxy01.merann.ru";
+		String proxyPort = "8080";				
+		System.setProperty("http.proxyHost", proxyAddr);
+		System.setProperty("http.proxyPort", proxyPort);
+		
+
+		// calling web service here ...
+		String ipAddress = "173.194.44.73"; // google - United States
+		
+		GeoIPService ipService = new GeoIPService();
+		
+		// this is our stub
+		GeoIPServiceSoap geoIPServiceSoap = ipService.getGeoIPServiceSoap();
+		
+		// stub.getCountryName(ipAddress);
+		GeoIP geoIp = geoIPServiceSoap.getGeoIP(ipAddress);
+		
+		// So all this is our client - we've done!
+		System.out.println(geoIp.getCountryName());			
+
 
 	}
 
